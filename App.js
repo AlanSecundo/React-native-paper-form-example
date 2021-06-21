2d032bf777e7cf71106d9e97abba3e4e7bdc9df0
Alan Secundo <alan_secundo@hotmail.com>
2021-06-20 21:54:21 -0300

    Criação de campos de cadastro de usuário, validação de campo de senha e confirmar senha

--- App.js
+++ App.js
@@ -1,28 +1,114 @@
+import axios from "axios";
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import { TextInput, Button, Snackbar } from "react-native-paper";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
-  const [name, setName] = useState('');
-
-  const changeNameInput = value => {
+  const changeNameInput = (value) => {
     setName(value);
-  }
+  };
+
+  const changeLastNameInput = (value) => {
+    setLastName(value);
+  };
+
+  const changeAge = (value) => {
+    setAge(value);
+  };
+
+  const changePassword = (value) => {
+    setPassword(value);
+  };
+  const changeConfirmPassword = (value) => {
+    setConfirmPassword(value);
+  };
+
+  const postUser = () => {
+    if (password !== confirmPassword) {
+      setVisible(true);
+      setHasError(true);
+      return;
+    }
+
+    setHasError(false);
+    axios.post(
+      "https://crudcrud.com/api/a555d0b58ac742e4a26f2c8be1741a60/usuario",
+      {
+        name: name,
+        lastName: lastName,
+        age: age,
+        password: password,
+      }
+    );
+  };
+
+  const onDismissSnackBar = () => {
+    setVisible(false);
+  };
 
   return (
     <View style={styles.container}>
-      <TextInput 
+      <TextInput
+        style={styles.marginTop}
         label="Primeiro nome"
+        placeholder="Digite seu nome"
         value={name}
         onChangeText={changeNameInput}
       />
-      <Button 
-        mode="contained"
-        onPress={() => console.log(name)}>
+      <TextInput
+        label="Sobrenome"
+        style={styles.marginTop}
+        placeholder="Digite seu sobrenome"
+        value={lastName}
+        onChangeText={changeLastNameInput}
+      />
+      <TextInput
+        label="Idade"
+        style={styles.marginTop}
+        placeholder="Digite sua idade"
+        value={age}
+        onChangeText={changeAge}
+      />
+      <TextInput
+        label="Senha"
+        style={styles.marginTop}
+        placeholder="Digite sua senha"
+        value={password}
+        onChangeText={changePassword}
+        secureTextEntry
+        error={hasError}
+      />
+      <TextInput
+        label="Confirmação de senha"
+        style={styles.marginTop}
+        placeholder="Confirme sua senha"
+        value={confirmPassword}
+        onChangeText={changeConfirmPassword}
+        secureTextEntry
+        error={hasError}
+      />
+      <Button mode="contained" style={styles.marginTop} onPress={postUser}>
         Enviar Informações
       </Button>
+      <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: "Fechar",
+          onPress: () => {},
+        }}
+      >
+        Suas senhas não conferem, verifique
+      </Snackbar>
     </View>
   );
 }
@@ -33,5 +119,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  marginTop: {
+    marginTop: "10px",
   },
 });
